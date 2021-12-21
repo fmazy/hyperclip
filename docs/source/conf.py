b8393c247738970ea35b902baf02d8bfd4465503
MAZY Francois-Remi <francois-remi.mazy@inria.fr>
2021-12-21 17:48:52 +0100

    fix doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,6 @@ extensions = [
     'sphinx.ext.autosectionlabel',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'numpydoc',
     "matplotlib.sphinxext.plot_directive",
     "IPython.sphinxext.ipython_directive",
     "IPython.sphinxext.ipython_console_highlighting",
